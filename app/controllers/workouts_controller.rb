0d14dde1a368e7bbad2f88a92c99e513c548b42b
NickisKnowledge <lifechangingstuff@gmail.com>
2019-04-24 19:19:16 -0500

    Generate Workout controller, make new workout  form & use params to create workout obj based on user input

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,41 @@
+class WorkoutsController < ApplicationController
+
+  # GET: /workouts
+  get "/workouts" do
+    erb :"/workouts/index.html"
+  end
+
+  # GET: /workouts/new
+  get "/workouts/new" do
+    erb :"/workouts/new.html"
+  end
+
+
+  # POST: /workouts
+  post "/workouts" do
+    workout = Workout.new(params)
+    workout.save
+    #binding.pry
+    redirect "/workouts/#{workout.id}"
+  end
+
+  # GET: /workouts/5
+  get "/workouts/:id" do
+    erb :"/workouts/show.html"
+  end
+
+  # GET: /workouts/5/edit
+  get "/workouts/:id/edit" do
+    erb :"/workouts/edit.html"
+  end
+
+  # PATCH: /workouts/5
+  patch "/workouts/:id" do
+    redirect "/workouts/:id"
+  end
+
+  # DELETE: /workouts/5/delete
+  delete "/workouts/:id/delete" do
+    redirect "/workouts"
+  end
+end
